9fdc60a78b10aa448a0cba255c9895b9a63ed6c9
Ida <97519183+Ida-3@users.noreply.github.com>
2022-01-18 12:20:52 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    Player1 player = new Player1("Link", "sword", 100);
+
+        player.damageByGun1();
+        player.damageByGun1();
+        player.damageByGun2();
+        player.damageByGun2();
+        player.heal();
+
+
+    }
+}
